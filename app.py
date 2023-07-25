d0a659262346a78b06ece8242b7fcd4391e3fe86
Caleb Curran-Velasco <calebcurran.velasco@gmail.com>
2023-07-25 17:46:42 -0600

    user api authentication fix

--- app.py
+++ app.py
@@ -104,8 +104,7 @@ def main():
             type="password",
             placeholder="Paste your OpenAI API key here (sk-...)",
             help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
-            value=os.environ.get("OPENAI_API_KEY", None)
-            or st.session_state.get("OPENAI_API_KEY", ""),
+            value=st.session_state.get("OPENAI_API_KEY", ""),
         )
 
         st.session_state["OPENAI_API_KEY"] = api_key_input
