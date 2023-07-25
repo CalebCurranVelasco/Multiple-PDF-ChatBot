14ab574dd9b33a292c3e74b2fa32be5852d92873
Caleb Curran-Velasco <calebcurran.velasco@gmail.com>
2023-07-25 00:21:12 -0600

    test

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def handle_userinput(user_question):
 def main():
     load_dotenv(find_dotenv())
 
-    st.set_page_config(page_title="ultiple PDFs ChatBot", page_icon=":books:")
+    st.set_page_config(page_title="Multiple PDFs ChatBot", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state: # initialize it here when using session state, can use this variable globally now and does not ever reset
